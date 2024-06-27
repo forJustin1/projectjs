974efc5b89bd1fce7f471e283558b2ea6dbaa4b1
Daniyar <Daniyar-1911@users.noreply.github.com>
2024-06-27 21:13:27 +0600

    дз1

---
+++ project/project/js/home_works.js
@@ -0,0 +1,30 @@
+const gmailInput = document.querySelector('#gmail_input')
+const gmailButton = document.querySelector('#gmail_button')
+const gmailResult = document.querySelector('#gmail_result')
+const regExp = /^\w{5,30}@gmail.com$/g
+
+gmailButton.onclick = () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    } else {
+        gmailResult.innerHTML = 'NOT OK'
+        gmailResult.style.color = 'red'
+    }
+}
+
+
+const childBlock = document.querySelector('.child_block')
+const parentBlock = document.querySelector('.parent_block')
+
+const mainWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+let moveLeft = 0
+const moveLeftChildBlock = () => {
+    if (moveLeft < mainWidth) {
+        moveLeft += 2
+        childBlock.style.left = `${moveLeft}px`
+        requestAnimationFrame(moveLeftChildBlock)
+    }
+}
+
+moveLeftChildBlock()
